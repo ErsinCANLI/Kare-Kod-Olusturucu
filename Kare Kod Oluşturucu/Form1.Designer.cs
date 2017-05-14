a242139634dc5339213167fb644b992f07bea9f0
Lütfi Can Say <lutficansay9@gmail.com>
2017-05-15 00:21:05 +0300

    C# Kare Kod Oluşturucu
    Devexpress Kullanılarak Yapılmıştır.

---
+++ Kare Kod Oluşturucu/Form1.Designer.cs
@@ -0,0 +1,325 @@
+﻿namespace Kare_Kod_Oluşturucu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.pctQRResim = new System.Windows.Forms.PictureBox();
+            this.simpleButton1 = new DevExpress.XtraEditors.SimpleButton();
+            this.groupControl1 = new DevExpress.XtraEditors.GroupControl();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.defaultLookAndFeel1 = new DevExpress.LookAndFeel.DefaultLookAndFeel(this.components);
+            this.simpleButton2 = new DevExpress.XtraEditors.SimpleButton();
+            this.simpleButton3 = new DevExpress.XtraEditors.SimpleButton();
+            this.barManager1 = new DevExpress.XtraBars.BarManager(this.components);
+            this.barDockControlTop = new DevExpress.XtraBars.BarDockControl();
+            this.barDockControlBottom = new DevExpress.XtraBars.BarDockControl();
+            this.barDockControlLeft = new DevExpress.XtraBars.BarDockControl();
+            this.barDockControlRight = new DevExpress.XtraBars.BarDockControl();
+            this.barStaticItem1 = new DevExpress.XtraBars.BarStaticItem();
+            this.barHeaderItem1 = new DevExpress.XtraBars.BarHeaderItem();
+            ((System.ComponentModel.ISupportInitialize)(this.pctQRResim)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.groupControl1)).BeginInit();
+            this.groupControl1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.barManager1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(53, 137);
+            this.textBox5.MaxLength = 25;
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(126, 21);
+            this.textBox5.TabIndex = 4;
+            // 
+            // pctQRResim
+            // 
+            this.pctQRResim.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pctQRResim.Image = ((System.Drawing.Image)(resources.GetObject("pctQRResim.Image")));
+            this.pctQRResim.InitialImage = ((System.Drawing.Image)(resources.GetObject("pctQRResim.InitialImage")));
+            this.pctQRResim.Location = new System.Drawing.Point(222, 24);
+            this.pctQRResim.Name = "pctQRResim";
+            this.pctQRResim.Size = new System.Drawing.Size(228, 201);
+            this.pctQRResim.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pctQRResim.TabIndex = 5;
+            this.pctQRResim.TabStop = false;
+            // 
+            // simpleButton1
+            // 
+            this.simpleButton1.Image = ((System.Drawing.Image)(resources.GetObject("simpleButton1.Image")));
+            this.simpleButton1.Location = new System.Drawing.Point(12, 24);
+            this.simpleButton1.Name = "simpleButton1";
+            this.simpleButton1.Size = new System.Drawing.Size(63, 27);
+            this.simpleButton1.TabIndex = 0;
+            this.simpleButton1.Text = "Oluştur";
+            this.simpleButton1.Click += new System.EventHandler(this.btnKareKod_Click);
+            // 
+            // groupControl1
+            // 
+            this.groupControl1.Controls.Add(this.label5);
+            this.groupControl1.Controls.Add(this.textBox4);
+            this.groupControl1.Controls.Add(this.label4);
+            this.groupControl1.Controls.Add(this.textBox3);
+            this.groupControl1.Controls.Add(this.label3);
+            this.groupControl1.Controls.Add(this.textBox5);
+            this.groupControl1.Controls.Add(this.textBox2);
+            this.groupControl1.Controls.Add(this.label2);
+            this.groupControl1.Controls.Add(this.textBox1);
+            this.groupControl1.Controls.Add(this.label1);
+            this.groupControl1.Location = new System.Drawing.Point(12, 57);
+            this.groupControl1.Name = "groupControl1";
+            this.groupControl1.Size = new System.Drawing.Size(201, 168);
+            this.groupControl1.TabIndex = 3;
+            this.groupControl1.Text = "Bilgiler";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(8, 140);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(39, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "5.Bilgi:";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(53, 110);
+            this.textBox4.MaxLength = 25;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(126, 21);
+            this.textBox4.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(8, 113);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(39, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "4.Bilgi:";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(53, 83);
+            this.textBox3.MaxLength = 25;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(126, 21);
+            this.textBox3.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(8, 86);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "3.Bilgi:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(53, 56);
+            this.textBox2.MaxLength = 25;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(126, 21);
+            this.textBox2.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(8, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "2.Bilgi:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(53, 29);
+            this.textBox1.MaxLength = 25;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(126, 21);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "1.Bilgi:";
+            // 
+            // defaultLookAndFeel1
+            // 
+            this.defaultLookAndFeel1.LookAndFeel.SkinName = "Seven Classic";
+            // 
+            // simpleButton2
+            // 
+            this.simpleButton2.Image = ((System.Drawing.Image)(resources.GetObject("simpleButton2.Image")));
+            this.simpleButton2.Location = new System.Drawing.Point(81, 24);
+            this.simpleButton2.Name = "simpleButton2";
+            this.simpleButton2.Size = new System.Drawing.Size(63, 27);
+            this.simpleButton2.TabIndex = 1;
+            this.simpleButton2.Text = "Kaydet";
+            this.simpleButton2.Click += new System.EventHandler(this.btnResimKayit_Click);
+            // 
+            // simpleButton3
+            // 
+            this.simpleButton3.Image = ((System.Drawing.Image)(resources.GetObject("simpleButton3.Image")));
+            this.simpleButton3.Location = new System.Drawing.Point(150, 24);
+            this.simpleButton3.Name = "simpleButton3";
+            this.simpleButton3.Size = new System.Drawing.Size(63, 27);
+            this.simpleButton3.TabIndex = 2;
+            this.simpleButton3.Text = "Temizle";
+            this.simpleButton3.Click += new System.EventHandler(this.simpleButton3_Click);
+            // 
+            // barManager1
+            // 
+            this.barManager1.DockControls.Add(this.barDockControlTop);
+            this.barManager1.DockControls.Add(this.barDockControlBottom);
+            this.barManager1.DockControls.Add(this.barDockControlLeft);
+            this.barManager1.DockControls.Add(this.barDockControlRight);
+            this.barManager1.Form = this;
+            this.barManager1.Items.AddRange(new DevExpress.XtraBars.BarItem[] {
+            this.barStaticItem1,
+            this.barHeaderItem1});
+            this.barManager1.MaxItemId = 2;
+            // 
+            // barDockControlTop
+            // 
+            this.barDockControlTop.CausesValidation = false;
+            this.barDockControlTop.Dock = System.Windows.Forms.DockStyle.Top;
+            this.barDockControlTop.Location = new System.Drawing.Point(0, 0);
+            this.barDockControlTop.Size = new System.Drawing.Size(464, 0);
+            // 
+            // barDockControlBottom
+            // 
+            this.barDockControlBottom.CausesValidation = false;
+            this.barDockControlBottom.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.barDockControlBottom.Location = new System.Drawing.Point(0, 257);
+            this.barDockControlBottom.Size = new System.Drawing.Size(464, 0);
+            // 
+            // barDockControlLeft
+            // 
+            this.barDockControlLeft.CausesValidation = false;
+            this.barDockControlLeft.Dock = System.Windows.Forms.DockStyle.Left;
+            this.barDockControlLeft.Location = new System.Drawing.Point(0, 0);
+            this.barDockControlLeft.Size = new System.Drawing.Size(0, 257);
+            // 
+            // barDockControlRight
+            // 
+            this.barDockControlRight.CausesValidation = false;
+            this.barDockControlRight.Dock = System.Windows.Forms.DockStyle.Right;
+            this.barDockControlRight.Location = new System.Drawing.Point(464, 0);
+            this.barDockControlRight.Size = new System.Drawing.Size(0, 257);
+            // 
+            // barStaticItem1
+            // 
+            this.barStaticItem1.Caption = "Bu program kodbankasi.gen.tr tarafından yapılmıştır ve tamamen ücretsizdir.";
+            this.barStaticItem1.Id = 0;
+            this.barStaticItem1.Name = "barStaticItem1";
+            this.barStaticItem1.TextAlignment = System.Drawing.StringAlignment.Near;
+            // 
+            // barHeaderItem1
+            // 
+            this.barHeaderItem1.Caption = "Bu program kodbankasi.gen.tr tarafından yapılmıştır ve tamamen ücretsizdir.";
+            this.barHeaderItem1.Id = 1;
+            this.barHeaderItem1.Name = "barHeaderItem1";
+            this.barHeaderItem1.ItemClick += new DevExpress.XtraBars.ItemClickEventHandler(this.barHeaderItem1_ItemClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(464, 257);
+            this.Controls.Add(this.simpleButton3);
+            this.Controls.Add(this.simpleButton2);
+            this.Controls.Add(this.groupControl1);
+            this.Controls.Add(this.simpleButton1);
+            this.Controls.Add(this.pctQRResim);
+            this.Controls.Add(this.barDockControlLeft);
+            this.Controls.Add(this.barDockControlRight);
+            this.Controls.Add(this.barDockControlBottom);
+            this.Controls.Add(this.barDockControlTop);
+            this.FormBorderEffect = DevExpress.XtraEditors.FormBorderEffect.Shadow;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Kare Kod Oluşturucu |||";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pctQRResim)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.groupControl1)).EndInit();
+            this.groupControl1.ResumeLayout(false);
+            this.groupControl1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.barManager1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.PictureBox pctQRResim;
+        private DevExpress.XtraEditors.SimpleButton simpleButton1;
+        private DevExpress.XtraEditors.GroupControl groupControl1;
+        private DevExpress.LookAndFeel.DefaultLookAndFeel defaultLookAndFeel1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private DevExpress.XtraEditors.SimpleButton simpleButton2;
+        private DevExpress.XtraEditors.SimpleButton simpleButton3;
+        private DevExpress.XtraBars.BarManager barManager1;
+        private DevExpress.XtraBars.BarStaticItem barStaticItem1;
+        private DevExpress.XtraBars.BarDockControl barDockControlTop;
+        private DevExpress.XtraBars.BarDockControl barDockControlBottom;
+        private DevExpress.XtraBars.BarDockControl barDockControlLeft;
+        private DevExpress.XtraBars.BarDockControl barDockControlRight;
+        private DevExpress.XtraBars.BarHeaderItem barHeaderItem1;
+
+    }
+}
+
---
+++ Kare Kod Oluşturucu/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Drawing;
+using System.Text;
+using System.Diagnostics;
+using System.Windows.Forms;
+using MessagingToolkit.QRCode.Codec.Data;
+using MessagingToolkit.QRCode.Codec;
+
+namespace Kare_Kod_Oluşturucu
+{
+    //lütficansay.com
+    public partial class Form1 : DevExpress.XtraEditors.XtraForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void btnKareKod_Click(object sender, EventArgs e)
+        {
+            StringBuilder bld = new StringBuilder();
+            if (textBox1.Text != "")
+            {
+                bld.Append(textBox1.Text);
+                bld.AppendLine();
+            }
+            if (textBox2.Text != "")
+            {
+                bld.Append(textBox2.Text);
+                bld.AppendLine();
+            }
+            if (textBox3.Text != "")
+            {
+                bld.Append(textBox3.Text);
+                bld.AppendLine();
+            }
+            if (textBox4.Text != "")
+            {
+                bld.Append(textBox4.Text);
+                bld.AppendLine();
+            }
+            if (textBox5.Text != "")
+            {
+                bld.Append(textBox5.Text);
+            }
+            pctQRResim.Image = QrCodeCreate(bld.ToString());
+        }
+
+        public Image QrCodeCreate(String CevrilecekOlan)
+        {
+            QRCodeEncoder encoder = new QRCodeEncoder();
+            Image img = encoder.Encode(CevrilecekOlan);
+            return img;
+        }
+
+        private void btnResimKayit_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveDialog = new SaveFileDialog();
+            saveDialog.InitialDirectory = Application.ExecutablePath;
+            saveDialog.Title = "Kare Kod";
+            saveDialog.Filter = "PNG Dosyaları(*.png)|*.png|JPEG dosyaları(*.jpg)|*.jpg|BMP dosyaları(*.bmp)|*.bmp";
+            saveDialog.DefaultExt = "*.png";
+            saveDialog.FileName = DateTime.Now.ToString("yyyy.MM.dd_hh.mm.ss");
+
+            if (saveDialog.ShowDialog() == DialogResult.OK)
+            {
+                pctQRResim.Image.Save(saveDialog.FileName);
+                Process.Start(saveDialog.FileName);
+            }
+        }
+
+        private void btnQrOku_Click(object sender, EventArgs e)
+        {
+            QRCodeDecoder decoder = new QRCodeDecoder();
+            Bitmap bmp = (Bitmap)pctQRResim.Image;
+            MessageBox.Show(decoder.decode(new QRCodeBitmapImage(bmp)));
+        }
+
+        private void simpleButton3_Click(object sender, EventArgs e)
+        {
+            pctQRResim.Image = pctQRResim.InitialImage;
+        }
+
+        private void barHeaderItem1_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+           
+        }
+    }
+}
